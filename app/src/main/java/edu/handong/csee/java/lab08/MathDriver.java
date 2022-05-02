4862ae91268a1ac8116089be072f353f990129b8
parkgyueun <parkgyueun@172.18.159.115>
2022-05-02 21:00:43 +0900

    add specific answers

--- app/src/main/java/edu/handong/csee/java/lab08/MathDriver.java
+++ app/src/main/java/edu/handong/csee/java/lab08/MathDriver.java
@@ -17,67 +17,67 @@ public class MathDriver {
 	        switch(args[1]){
 	            case "Max":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Max Answer: " + 
 	                                    mathCalculator.getMax(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Min":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Min Answer: " + 
 	                                    mathCalculator.getMin(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Abs":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Abs Answer: " + 
 	                                    mathCalculator.getAbs(Integer.parseInt(args[2])));
 	                break;
 	            case "Sum":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Sum Answer: " + 
 	                                    mathCalculator.getSum(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Diff":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Diff Answer: " + 
 	                                    mathCalculator.getDiff(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Product":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Product Answer: " + 
 	                                    mathCalculator.getProduct(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Quotient":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Quotient Answer: " + 
 	                                    mathCalculator.getQuotient(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Remainder":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Remainder Answer: " + 
 	                                    mathCalculator.getRemainder(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Power":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Power Answer: " + 
 	                                    mathCalculator.getPower(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Factorial":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Factorial Answer: " + 
 	                                    mathCalculator.getFactorial(Integer.parseInt(args[2])));
 	                break;
 	            case "Gcd":
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Gcd Answer: " + 
 	                                    mathCalculator.getGcd(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Lcm":                
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Lcm Answer: " + 
 	                                    mathCalculator.getLcm(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
 	                break;
 	            case "Square":                
 	                System.out.println("Name: " + mathCalculator.getName() + 
-	                                    ", Answer: " + 
+	                                    ", Square Answer: " + 
 	                                    mathCalculator.getSquare(Integer.parseInt(args[2])));
 	                break;
 	            default: 
